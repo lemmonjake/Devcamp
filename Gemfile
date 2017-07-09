ca6a290de5f7edbd4399b8f07ac26817a01f1c41
Jordan Hudgens <jordan@devcamp.com>
2017-07-09 00:53:32 +0000

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', '~> 4.0.0.alpha6' 
+
+gem 'jake_lemmon_view_tool', git: 'https://github.com/lemmonjake/jake_lemmon_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,10 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    JakeLemmonViewTool::Renderer.copyright 'Jake Lemmon', 'All rights reserved'
+  end
+  
 end
 
